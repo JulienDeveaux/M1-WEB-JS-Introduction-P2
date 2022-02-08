9d97fecb217b4c8c41482f70066865990ff01450
Julien Deveaux <julien.deveaux@hotmail.com>
2022-02-08 09:32:33 +0100

    Add sensor class

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 export const version = () => '1.0.0';
 
-/* TODO : Créer le modèle objet ici */
+const myEnum = [
+  'THIS',
+  'THAT',
+  'OTHER'
+];
+
+export class Sensor {
+  #id;
+  #name;
+
+  constructor(id = 0, name = "new Sensor") {
+    this.#id = id;
+    this.#name = name;
+
+  }
+
+  get id() {
+    return this.#id;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  set id(id) {
+    if(typeof id !== "number") {
+      throw new Error("id must be a number");
+    }
+    if(id < 0) {
+      throw new Error("id must be positive");
+    }
+    this.#id = id;
+  }
+
+  set name(name) {
+    if(typeof name !== "string") {
+      throw new Error("name must ba a string");
+    }
+    this.#name = name;
+  }
+}
--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,6 @@
 const fs = require('fs').promises;
 
-import { version } from '.';
+import {Sensor, version} from '.';
 
 let data;
 beforeAll(async () => {
@@ -19,6 +19,39 @@ describe('Sensor model tests', () => {
       expect(version()).toBe('1.0.0');
     });
   });
-  /* TODO: Écrire ici la suite de tests pour le modèle objet.*/
-  //
+
+  describe('Sensor class tests', () => {
+    test('constructor', () => {
+      let s = new Sensor();
+      expect(s).toBeDefined();
+      expect(Object.getPrototypeOf(s)).toBe(Sensor.prototype);
+    });
+
+    test('id write with positive number', () => {
+      let s = new Sensor();
+      s.id = 10;
+      expect(s.id).toBe(10);
+    });
+
+    test('id not a number', () => {
+      let s = new Sensor();
+      expect(() => (s.id = "myId")).toThrow();
+    });
+
+    test('negative id', () => {
+      let s = new Sensor();
+      expect(() => (s.id = -1)).toThrow();
+    });
+
+    test('name write with a valid string', () => {
+      let s = new Sensor();
+      s.name = "MyName";
+      expect(s.name).toBe("MyName");
+    });
+
+    test('name not a string', () => {
+      let s = new Sensor();
+      expect(() => (s.name = 0)).toThrow();
+    });
+  });
 });
